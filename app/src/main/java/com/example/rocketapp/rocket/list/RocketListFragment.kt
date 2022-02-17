4f33eb55693e0d909b52fbaa15af988906930dd1
Martin Beran <martinrohatej@gmail.com>
2022-02-17 13:51:27 +0100

    WIP: view binding abstraction

--- app/src/main/java/com/example/rocketapp/rocket/list/RocketListFragment.kt
+++ app/src/main/java/com/example/rocketapp/rocket/list/RocketListFragment.kt
@@ -1,6 +1,29 @@
 package com.example.rocketapp.rocket.list
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.rocketapp.databinding.FragmentRocketListBinding
 import com.example.rocketapp.tools.BaseFragment
 
 class RocketListFragment: BaseFragment() {
+
+    private var _binding: FragmentRocketListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = FragmentRocketListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
--- app/src/main/java/com/example/rocketapp/tools/BaseFragment.kt
+++ app/src/main/java/com/example/rocketapp/tools/BaseFragment.kt
@@ -1,6 +1,30 @@
 package com.example.rocketapp.tools
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+import com.example.rocketapp.databinding.FragmentRocketListBinding
 
-abstract class BaseFragment: Fragment() {
+abstract class BaseFragment<Binding: ViewBinding>: Fragment() {
+
+    private var _binding: Binding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = Binding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }

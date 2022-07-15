78ee54f32512a233daf723011c2037100cf148b5
janveprek <janveprek@gmail.com>
2022-07-15 11:02:10 +0200

    FEAT: updated styles

--- app/src/main/java/com/veprek/honza/rocketlaunch/ui/theme/Color.kt
+++ app/src/main/java/com/veprek/honza/rocketlaunch/ui/theme/Color.kt
@@ -2,8 +2,11 @@ package com.veprek.honza.rocketlaunch.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 val Pink = Color(0xFFFF69B4)
+val Orange = Color(0xFFF44336)
+val LightGray = Color(0xFFE8E8E8)
+val backgroundLight = Color(0xFFF3F3F3)
+val backgroundDark = Color(0xFF1F2024)

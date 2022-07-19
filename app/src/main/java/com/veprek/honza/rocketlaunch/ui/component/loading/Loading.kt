cb984ee1b4de3080a968e33266c43759ca901436
janveprek <janveprek@gmail.com>
2022-07-19 11:43:07 +0200

    FEAT: Added loading component

---
+++ app/src/main/java/com/veprek/honza/rocketlaunch/ui/component/loading/Loading.kt
@@ -0,0 +1,20 @@
+package com.veprek.honza.rocketlaunch.ui.component.loading
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun Loading() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator()
+    }
+}

334671ef92b3b181753c5b7040766b03e397d12a
janveprek <janveprek@gmail.com>
2022-07-19 11:41:55 +0200

    FEAT: Added logging

--- app/src/main/java/com/veprek/honza/rocketlaunch/App.kt
+++ app/src/main/java/com/veprek/honza/rocketlaunch/App.kt
@@ -2,6 +2,19 @@ package com.veprek.honza.rocketlaunch
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import quanti.com.kotlinlog.Log
+import quanti.com.kotlinlog.android.AndroidLogger
+import quanti.com.kotlinlog.base.LogLevel
+import quanti.com.kotlinlog.base.LoggerBundle
 
 @HiltAndroidApp
-class App : Application()
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.initialise(this)
+
+        val androidBundle = LoggerBundle(LogLevel.DEBUG)
+        Log.addLogger(AndroidLogger(androidBundle))
+    }
+}

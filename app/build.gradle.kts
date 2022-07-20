5e0c71f6a076e3c68644180c6f0ed658467789b6
janveprek <janveprek@gmail.com>
2022-07-20 10:28:10 +0200

    FEAT: Added Room dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,6 +19,7 @@ val ktlint: Configuration by configurations.creating
 
 // library version
 val composeVersion = "1.1.1"
+val roomVersion = "2.4.2"
 val navVersion = "2.5.0"
 val hiltVersion = "2.42"
 
@@ -94,6 +95,11 @@ dependencies {
     implementation("com.squareup.moshi:moshi-kotlin:1.13.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.4.0")
 
+    // Room
+    implementation("androidx.room:room-runtime:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+    annotationProcessor("androidx.room:room-compiler:$roomVersion")
+    kapt("androidx.room:room-compiler:$roomVersion")
     // Testing
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")

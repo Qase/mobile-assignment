44cbedc4f62e435b2a46f9d7666f0328f5f091ec
janveprek <janveprek@gmail.com>
2022-07-21 11:01:26 +0200

    FEAT: Added error and loading components

--- app/src/main/java/com/veprek/honza/rocketlaunch/ui/component/error/Error.kt
+++ app/src/main/java/com/veprek/honza/rocketlaunch/ui/component/error/Error.kt
@@ -1,11 +1,28 @@
 package com.veprek.honza.rocketlaunch.ui.component.error
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.veprek.honza.rocketlaunch.R
+import com.veprek.honza.rocketlaunch.ui.theme.horizontalPadding
+import com.veprek.honza.rocketlaunch.ui.theme.verticalPadding
 
 @Composable
 fun Error() {
-    Text(stringResource(R.string.error))
+    Column(
+        Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .padding(horizontal = horizontalPadding),
+        verticalArrangement = Arrangement.spacedBy(verticalPadding)
+    ) {
+        Text(stringResource(R.string.error))
+    }
 }
--- app/src/main/java/com/veprek/honza/rocketlaunch/ui/component/loading/Loading.kt
+++ app/src/main/java/com/veprek/honza/rocketlaunch/ui/component/loading/Loading.kt
@@ -1,9 +1,6 @@
 package com.veprek.honza.rocketlaunch.ui.component.loading
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
@@ -14,16 +11,11 @@ import androidx.compose.ui.tooling.preview.Preview
 @Preview
 @Composable
 fun Loading() {
-    Column(
+    Box(
         Modifier
-            .fillMaxHeight(),
-        verticalArrangement = Arrangement.Center
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
     ) {
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            CircularProgressIndicator()
-        }
+        CircularProgressIndicator()
     }
 }

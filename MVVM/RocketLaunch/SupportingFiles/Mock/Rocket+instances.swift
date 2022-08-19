656b88fe0ff402beeabc76d2d4c64fb46057b0ff
havrdova <havrdova1lucie@gmail.com>
2022-08-19 17:27:46 +0200

    FIX: date creation from string

--- MVVM/RocketLaunch/SupportingFiles/Mock/Rocket+instances.swift
+++ MVVM/RocketLaunch/SupportingFiles/Mock/Rocket+instances.swift
@@ -20,24 +20,38 @@ extension Rocket {
     static let falcon1 = Rocket(
         id: "1",
         name: "Falcon 1",
-        firstFlight: Date("24.3.2006", stringFormat: "dd.MM.yyy")
+        firstFlight: createDate("24.3.2006", stringFormat: "dd.MM.yyy")
     )
 
     static let falcon9 = Rocket(
         id: "2",
         name: "Falcon 9",
-        firstFlight: Date("6.4.2010", stringFormat: "dd.MM.yyy")
+        firstFlight: createDate("6.4.2010", stringFormat: "dd.MM.yyy")
     )
 
     static let falconHeavy = Rocket(
         id: "3",
         name: "Falcon Heavy",
-        firstFlight: Date("2.6.2018", stringFormat: "dd.MM.yyy")
+        firstFlight: createDate("2.6.2018", stringFormat: "dd.MM.yyy")
     )
 
     static let starship = Rocket(
         id: "4",
         name: "Starship",
-        firstFlight: Date("12.1.2021", stringFormat: "dd.MM.yyy")
+        firstFlight: createDate("12.1.2021", stringFormat: "dd.MM.yyy")
     )
+}
+
+fileprivate func createDate(
+    _ date: String,
+    stringFormat: String
+) -> Date {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = stringFormat
+
+    guard let createdDate = dateFormatter.date(from: date) else {
+        fatalError("Cannot convert date")
+    }
+
+    return createdDate
 }

06b76dca3d9d32cc802216fd21ea5e6c5cae3f6c
havrdova <havrdova1lucie@gmail.com>
2022-08-19 14:46:49 +0200

    FEAT: TCA app created

---
+++ TCA/TCA/TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAApp.swift
+//  TCA
+//
+//  Created by Lucie Havrdová on 19.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

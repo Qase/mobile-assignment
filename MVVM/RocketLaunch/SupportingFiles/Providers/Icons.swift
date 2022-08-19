9c794eb069f1b8fa0ef8cd660f0a2866de42a6fc
havrdova <havrdova1lucie@gmail.com>
2022-08-19 17:39:32 +0200

    FEAT: Icons provider

---
+++ MVVM/RocketLaunch/SupportingFiles/Providers/Icons.swift
@@ -0,0 +1,22 @@
+//
+//  Icons.swift
+//  RocketLaunch
+//
+//  Created by Lucie Havrdová on 19.08.2022.
+//
+
+import SwiftUI
+
+enum Icons: String {
+    case burn = "Burn"
+    case engine = "Engine"
+    case fuel = "Fuel"
+    case reusable = "Reusable"
+    case flyingRocket = "Rocket Flying"
+    case idleRocket = "Rocket Idle"
+    case rocket = "Rocket"
+
+    var image: Image {
+        Image(self.rawValue)
+    }
+}

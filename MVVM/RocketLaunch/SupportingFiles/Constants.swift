1e2fba5dc802fa856a85a1326d4360c1d0ddc06a
havrdova <havrdova1lucie@gmail.com>
2022-08-24 11:43:01 +0200

    FEAT: Constants file changed

--- MVVM/RocketLaunch/SupportingFiles/Constants.swift
+++ MVVM/RocketLaunch/SupportingFiles/Constants.swift
@@ -9,5 +9,15 @@ import Foundation
 import SwiftUI
 
 struct Constants {
-    static let cornerRadius: CGFloat = 10
+    enum View {
+        static let cornerRadius: CGFloat = 10
+    }
+
+    enum Networking {
+        static let basicURL = "https://api.spacexdata.com"
+        static let allRockets = "/v4/rockets"
+        static func oneRocket(id: String) -> String {
+            return "/v4/rockets/" + id
+        }
+    }
 }

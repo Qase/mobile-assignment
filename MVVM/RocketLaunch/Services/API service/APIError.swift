2d056e584a73bf7d93cd9ee1fba5bbd347fe71b8
havrdova <havrdova1lucie@gmail.com>
2022-08-24 14:24:34 +0200

    FEAT: API Error description

--- MVVM/RocketLaunch/Services/API service/APIError.swift
+++ MVVM/RocketLaunch/Services/API service/APIError.swift
@@ -11,4 +11,21 @@ enum APIError: Error {
     case downloadError
     case decodingError
     case rocketNotFound
+
+    case invalidURL
+}
+
+extension APIError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .downloadError:
+            return NSLocalizedString("Description of downloading error.", comment: "It was unable to download data.")
+        case .decodingError:
+            return NSLocalizedString("Description of not decodable data.", comment: "Data were not decoded.")
+        case .rocketNotFound:
+            return NSLocalizedString("Description of not found rocket.", comment: "Rocket was not found.")
+        case .invalidURL:
+            return NSLocalizedString("Description of invalid URL.", comment: "Invalid API endpoint.")
+        }
+    }
 }

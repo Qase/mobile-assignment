f1efe9b54fd569963dbceb813aba00fb66639b19
Cyril Colombier <>
2024-12-17 15:05:48 +0100

    Fix checkRocketNames test

--- Solution/iOS/RocketAppUITests/Screens/RocketListScreen.swift
+++ Solution/iOS/RocketAppUITests/Screens/RocketListScreen.swift
@@ -7,14 +7,14 @@ struct RocketListScreen: Screen {
   let app: XCUIApplication
 
   private let rocketCells: XCUIElementQuery
-  private let rocketNameLabels: XCUIElementQuery
+  private let rocketNames: XCUIElementQuery
   private let rocketCellArrows: XCUIElementQuery
   
   init(app: XCUIApplication) {
     self.app = app
     
     rocketCells = app.buttons.matching(identifier: AccessibilityKeys.RocketList.rocketCell)
-    rocketNameLabels = app.staticTexts.matching(identifier: AccessibilityKeys.RocketListCell.rocketNameLabel)
+    rocketNames = app.staticTexts.matching(identifier: AccessibilityKeys.RocketListCell.rocketNameLabel)
     rocketCellArrows = app.images.matching(identifier: AccessibilityKeys.RocketListCell.rocketCellArrow)
   }
 
@@ -24,16 +24,17 @@ struct RocketListScreen: Screen {
     print("Number of rocket cells displayed: \(rocketCells.count)")
     XCTAssert(rocketCells.count == 4, "Expected exactly 4 rocket cells to be displayed.")
     return self
-    }
+  }
   
   @discardableResult
-  func checkRocketNames() -> Self {
-    let expectedRocketNames = ["Falcon 1", "Falcon 9", "Falcon Heavy", "Starship"]
-    for index in 0..<rocketCells.count {
-     let rocketName = rocketNameLabels.element(boundBy: index)
+  func checkRocketNames(_ expectedRocketNames: [String]) -> Self {
+    for index in 0..<rocketNames.count {
+      let rocketName = rocketNames.element(boundBy: index)
       let expectedRocketName = expectedRocketNames[index]
-      XCTAssert(rocketName.label == expectedRocketName,
-                "Rocket name \(expectedRocketName) does not match the expected value.")
+      XCTAssert(
+        rocketName.label == expectedRocketName,
+        "Rocket name at index \(index) does not match the expected value"
+      )
     }
     return self
   }

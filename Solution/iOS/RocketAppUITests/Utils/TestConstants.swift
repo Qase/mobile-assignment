aa0c6d3a687ec56b21f7b27418cf9837f253862e
Cyril Colombier <>
2024-12-30 14:01:19 +0100

    Fix test constants Add timeout constant

--- Solution/iOS/RocketAppUITests/Utils/TestConstants.swift
+++ Solution/iOS/RocketAppUITests/Utils/TestConstants.swift
@@ -1,11 +1,12 @@
 import Foundation
 
 enum TestConstants {
-  enum LoginDetails{
-   public static let username = "astronaut1"
-   public static let password = "space"
+  enum LoginCredentials {
+    static let username = "astronaut1"
+    static let password = "space"
   }
-  enum Rockets {
-    public static let rocketNames = ["Falcon 1", "Falcon 9", "Falcon Heavy", "Starship"]
+
+  enum Timeouts {
+    static let defaultTimeout: Double = 5
   }
 }

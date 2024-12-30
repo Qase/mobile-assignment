ecf4574da5fe0a8439d5077db2f2570ed22b4550
Cyril Colombier <>
2024-12-30 13:59:33 +0100

    Remove check rocket count and names functions Fix goToRocket function

--- Solution/iOS/RocketAppUITests/Screens/RocketListScreen.swift
+++ Solution/iOS/RocketAppUITests/Screens/RocketListScreen.swift
@@ -6,42 +6,18 @@ import XCTest
 struct RocketListScreen: Screen {
   let app: XCUIApplication
 
-  private let rocketCells: XCUIElementQuery
-  private let rocketNames: XCUIElementQuery
   private let rocketCellArrows: XCUIElementQuery
-  
+
   init(app: XCUIApplication) {
     self.app = app
-    
-    rocketCells = app.buttons.matching(identifier: AccessibilityKeys.RocketList.rocketCell)
-    rocketNames = app.staticTexts.matching(identifier: AccessibilityKeys.RocketListCell.rocketNameLabel)
-    rocketCellArrows = app.images.matching(identifier: AccessibilityKeys.RocketListCell.rocketCellArrow)
+
+    rocketCellArrows = app.images.matching(identifier: AccessibilityKeys.RocketListCell.rocketArrowImage)
   }
 
   @discardableResult
-  func countRocketCells() -> Self {
-    XCTAssert(rocketCells.firstMatch.waitForExistence(timeout: 5), "Rocket cells did not appear within the timeout.")
-    print("Number of rocket cells displayed: \(rocketCells.count)")
-    XCTAssert(rocketCells.count == 4, "Expected exactly 4 rocket cells to be displayed.")
-    return self
-  }
-  
-  @discardableResult
-  func checkRocketNames(_ expectedRocketNames: [String]) -> Self {
-    for index in 0..<rocketNames.count {
-      let rocketName = rocketNames.element(boundBy: index)
-      let expectedRocketName = expectedRocketNames[index]
-      XCTAssert(
-        rocketName.label == expectedRocketName,
-        "Rocket name at index \(index) does not match the expected value"
-      )
-    }
-    return self
-  }
-  
-  @discardableResult
-  func tapFirstRocketCell() -> Self {
-    let firstRocketCell = rocketCellArrows.element(boundBy: 0)
+  func goToRocket(atIndex index: Int) -> Self {
+    let firstRocketCell = rocketCellArrows.element(boundBy: index)
+    XCTAssert(firstRocketCell.waitForExistence(timeout: TestConstants.Timeouts.defaultTimeout))
     firstRocketCell.tap()
     return self
   }

b73e89b0541aea16a0db5e141960ba252354158d
Cyril Colombier <>
2025-01-13 18:30:42 +0100

    REFACTOR rename file

--- Solution/iOS/RocketApp/Features/Sources/RocketDetail/AccessibilityKeys+RocketDetail.swift
+++ Solution/iOS/RocketApp/Features/Sources/RocketDetail/AccessibilityKeys+RocketDetail.swift
@@ -2,7 +2,7 @@ import UIToolkit
 
 extension AccessibilityKeys {
   public enum RocketDetail {
-    public static let rocketTitleText = "rocketTitleTextID"
+    public static let titleStaticText = "titleStaticTextID"
     public static let launchButton = "launchButtonID"
   }
 }
